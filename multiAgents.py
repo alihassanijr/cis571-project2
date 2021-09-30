da4dc3788835f9031af01fe7dd77fb586a31d69a
alih <alihassanijr1998@gmail.com>
2021-09-30 15:46:44 -0700

    Completed solution

--- multiAgents.py
+++ multiAgents.py
@@ -11,12 +11,20 @@
 # Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
-
+from typing import List
 from util import manhattanDistance
 from game import Directions
 import random, util
 
 from game import Agent
+
+
+INF = 999999999
+
+
+def mean(x: List) -> float:
+    return sum(x) / len(x)
+
 
 class ReflexAgent(Agent):
     """
@@ -68,13 +76,55 @@ class ReflexAgent(Agent):
         """
         # Useful information you can extract from a GameState (pacman.py)
         successorGameState = currentGameState.generatePacmanSuccessor(action)
+        oldPos = currentGameState.getPacmanPosition()
         newPos = successorGameState.getPacmanPosition()
         newFood = successorGameState.getFood()
         newGhostStates = successorGameState.getGhostStates()
         newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
 
-        "*** YOUR CODE HERE ***"
-        return successorGameState.getScore()
+        if oldPos == newPos:
+            return -INF
+
+        currentFood = currentGameState.getFood()
+        newGhostPos = [g.configuration.pos for g in newGhostStates]
+        newScaredGhostPos = [g.configuration.pos for g in newGhostStates if g.scaredTimer > 0]
+        newRegularGhostPos = [g.configuration.pos for g in newGhostStates if g.scaredTimer == 0]
+
+        food = [(i, j) for i in range(len(currentFood.data)) for j in range(len(currentFood.data[i])) if currentFood.data[i][j]]
+        successor_food = [(i, j) for i in range(len(newFood.data)) for j in range(len(newFood.data[i])) if newFood.data[i][j]]
+
+        if len(successor_food) == 0:
+            return INF
+
+        foods_eaten = len(successor_food) - len(food)
+
+        aghost2food_dist = [manhattanDistance(g, f) if newScaredTimes[i] == 0 else 9999999 for f in successor_food for i, g in enumerate(newGhostPos)]
+
+        ghost2food_dist = [manhattanDistance(g, f) for f in successor_food for g in newRegularGhostPos]
+        scaredghost2food_dist = [manhattanDistance(g, f) for f in successor_food for g in newScaredGhostPos]
+
+        ghost2pac_dist = [manhattanDistance(g, newPos) for g in newRegularGhostPos]
+        scaredghost2pac_dist = [manhattanDistance(g, newPos) for g in newScaredGhostPos]
+
+        pac2food_dist = [manhattanDistance(f, newPos) for f in successor_food]
+
+        reachable_foods = [pac2food_dist[i] for i, f in enumerate(successor_food) if aghost2food_dist[i] > pac2food_dist[i]]
+
+        if newPos in food and (len(ghost2pac_dist) == 0 or min(ghost2pac_dist) > 4):
+            return INF
+
+        if len(scaredghost2pac_dist) > 0 and min(scaredghost2pac_dist) < 1:
+            return INF
+
+        score = currentGameState.getScore()
+        score += 0 if len(ghost2pac_dist) == 0 else max(ghost2pac_dist)
+        score += 0 if len(scaredghost2pac_dist) == 0 else min(scaredghost2pac_dist)
+        score += 0 if len(ghost2food_dist) == 0 else max(ghost2food_dist)
+        score += 0 if len(scaredghost2food_dist) == 0 else min(scaredghost2food_dist)
+        score += foods_eaten
+        score += 0 if len(reachable_foods) == 0 else 25 / min(reachable_foods)
+
+        return score
 
 def scoreEvaluationFunction(currentGameState):
     """
@@ -113,29 +163,34 @@ class MinimaxAgent(MultiAgentSearchAgent):
 
     def getAction(self, gameState):
         """
-        Returns the minimax action from the current gameState using self.depth
-        and self.evaluationFunction.
-
-        Here are some method calls that might be useful when implementing minimax.
-
-        gameState.getLegalActions(agentIndex):
-        Returns a list of legal actions for an agent
-        agentIndex=0 means Pacman, ghosts are >= 1
-
-        gameState.generateSuccessor(agentIndex, action):
-        Returns the successor game state after an agent takes an action
-
-        gameState.getNumAgents():
-        Returns the total number of agents in the game
-
-        gameState.isWin():
-        Returns whether or not the game state is a winning state
-
-        gameState.isLose():
-        Returns whether or not the game state is a losing state
+        Returns the minimax action from the current gameState.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        n_ghosts = gameState.getNumAgents() - 1
+        return self.pacman_move(gameState, n_ghosts=n_ghosts, return_action=True)
+
+    def pacman_move(self, state, n_ghosts, depth=0, return_action=False):
+        if depth == self.depth:
+            return self.evaluationFunction(state)
+        if state.isWin() or state.isLose():
+            return self.evaluationFunction(state)
+        legalMoves = state.getLegalActions(0)
+        values = {move: self.ghost_move(state.generateSuccessor(0, move), n_ghosts=n_ghosts, depth=depth) for move in legalMoves}
+        max_idx = max(values, key=values.get)
+        if return_action:
+            return max_idx
+        return values[max_idx]
+
+    def ghost_move(self, state, agent=0, n_ghosts=1, depth=0):
+        ghost = agent + 1
+        if ghost > n_ghosts:
+            return self.pacman_move(state, depth=depth + 1, n_ghosts=n_ghosts)
+        if state.isWin() or state.isLose():
+            return self.evaluationFunction(state)
+        legalMoves = state.getLegalActions(ghost)
+        values = {move: self.ghost_move(state.generateSuccessor(ghost, move), agent=agent + 1, depth=depth, n_ghosts=n_ghosts) for move in legalMoves}
+        min_idx = min(values, key=values.get)
+        return values[min_idx]
+
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
     """
@@ -146,8 +201,41 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         """
         Returns the minimax action using self.depth and self.evaluationFunction
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        n_ghosts = gameState.getNumAgents() - 1
+        return self.pacman_move(gameState, n_ghosts=n_ghosts, return_action=True)
+
+    def pacman_move(self, state, n_ghosts, alpha=-INF, beta=INF, depth=0, return_action=False):
+        if depth == self.depth:
+            return self.evaluationFunction(state)
+        if state.isWin() or state.isLose():
+            return self.evaluationFunction(state)
+        mx, mxm = -INF, None
+        legalMoves = state.getLegalActions(0)
+        for move in legalMoves:
+            v = self.ghost_move(state.generateSuccessor(0, move), alpha=alpha, beta=beta, n_ghosts=n_ghosts, depth=depth)
+            alpha = max(alpha, v)
+            mx, mxm = (v, move) if v > mx else (mx, mxm)
+            if v > beta:
+                return v
+        if return_action:
+            return mxm
+        return mx
+
+    def ghost_move(self, state, alpha, beta, agent=0, n_ghosts=1, depth=0):
+        ghost = agent + 1
+        if ghost > n_ghosts:
+            return self.pacman_move(state, alpha=alpha, beta=beta, depth=depth + 1, n_ghosts=n_ghosts)
+        if state.isWin() or state.isLose():
+            return self.evaluationFunction(state)
+        legalMoves = state.getLegalActions(ghost)
+        mn = INF
+        for move in legalMoves:
+            v = self.ghost_move(state.generateSuccessor(ghost, move), alpha=alpha, beta=beta, agent=agent + 1, depth=depth, n_ghosts=n_ghosts)
+            beta = min(beta, v)
+            mn = min(mn, v)
+            if v < alpha:
+                return v
+        return mn
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
     """
@@ -161,18 +249,105 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
         All ghosts should be modeled as choosing uniformly at random from their
         legal moves.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        n_ghosts = gameState.getNumAgents() - 1
+        return self.pacman_move(gameState, n_ghosts=n_ghosts, return_action=True)
+
+    def pacman_move(self, state, n_ghosts, depth=0, return_action=False):
+        if depth == self.depth:
+            return self.evaluationFunction(state)
+        if state.isWin() or state.isLose():
+            return self.evaluationFunction(state)
+        legalMoves = state.getLegalActions(0)
+        values = {move: self.ghost_move(state.generateSuccessor(0, move), n_ghosts=n_ghosts, depth=depth) for move in
+                  legalMoves}
+        max_idx = max(values, key=values.get)
+        if return_action:
+            return max_idx
+        return values[max_idx]
+
+    def ghost_move(self, state, agent=0, n_ghosts=1, depth=0):
+        ghost = agent + 1
+        if ghost > n_ghosts:
+            return self.pacman_move(state, depth=depth + 1, n_ghosts=n_ghosts)
+        if state.isWin() or state.isLose():
+            return self.evaluationFunction(state)
+        legalMoves = state.getLegalActions(ghost)
+        values = [self.ghost_move(state.generateSuccessor(ghost, move), agent=agent + 1, depth=depth, n_ghosts=n_ghosts) for move in legalMoves]
+        return mean(values)
+
 
 def betterEvaluationFunction(currentGameState):
     """
     Your extreme ghost-hunting, pellet-nabbing, food-gobbling, unstoppable
     evaluation function (question 5).
 
-    DESCRIPTION: <write something here so we know what you did>
+    DESCRIPTION:
+    Basically a combination of the food left on the map, with respect to map size
+    while encouraging following scared ghosts and avoiding ones that are not scared.
     """
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    from numpy import array as arr
+    pac = currentGameState.getPacmanPosition()
+    food_mat = currentGameState.getFood().data
+    wall_mat = currentGameState.getWalls().data
+    num_cells = arr(not wall_mat).sum()
+    capsule_mat = currentGameState.getCapsules()
+    ghost_states = currentGameState.getGhostStates()
+
+    regular_ghosts = [g.configuration.pos for g in ghost_states if g.scaredTimer == 0]
+    scared_ghosts = [g.configuration.pos for g in ghost_states if g.scaredTimer > 0]
+    food = [(i, j) for i in range(len(food_mat)) for j in range(len(food_mat[i])) if food_mat[i][j]]
+    capsules = [(i, j) for i in range(len(capsule_mat)) for j in range(len(capsule_mat[i])) if capsule_mat[i][j]]
+
+    if len(food) == 0:
+        # Win state!
+        return INF
+
+    pac2food = arr([manhattanDistance(f, pac) for f in food])
+
+    pac2ghost = arr([manhattanDistance(g, pac) for g in regular_ghosts])
+    nearest_ghost = -1 if len(pac2ghost) <= 0 else pac2ghost.argmin()
+
+    pac2scaredghost = arr([manhattanDistance(g, pac) for g in scared_ghosts])
+    nearest_scaredghost = -1 if len(pac2scaredghost) <= 0 else pac2scaredghost.argmin()
+
+    scared_timers = arr([g.scaredTimer for g in ghost_states if g.scaredTimer > 0])
+
+    # Start with the actual score
+    score = currentGameState.getScore()
+
+    # Deduct points for sum of distances between pacman and food points
+    # If the food on one side of the map is still not taken, pacman better make a run for it.
+    score -= sum(pac2food)
+    # Add points for the number of cells that are empty (food taken) -- maximum when there's no food left
+    score += num_cells / len(food)
+
+    # If there is a scared ghost, add points for their timer
+    # (max steps that can be taken to hit the scared ghost)
+    # and deduct points for the sum of their distances.
+    if nearest_scaredghost >= 0:
+        score += sum(scared_timers) - pac2scaredghost.sum()
+
+    # If there is a normal ghost, add points for avoid the nearest one
+    # and add points for capsules (eat a capsule and kill the ghost)
+    if nearest_ghost >= 0:
+        score += pac2ghost[nearest_ghost]
+        score += arr(not capsule_mat).sum() - len(capsules)
+
+    return score
+
 
 # Abbreviation
 better = betterEvaluationFunction
+
+# ghosts = [g.configuration.pos for g in ghost_states]
+# nearest_food = pac2food.argmin()
+# food_dist_avg = 1 / pac2food.sum()
+
+# pac2capsule = arr([manhattanDistance(c, pac) for c in capsules])
+# nearest_capsule = -1 if len(pac2capsule) <= 0 else pac2capsule.argmin()
+
+# ghost2food = arr([[manhattanDistance(f, g) for f in food] for g in ghosts])
+# ghost2capsule = arr([[manhattanDistance(c, g) for c in capsules] for g in ghosts])
+
+# p2f = arr([[manhattanDistance(f1, f2) for f1 in food] for f2 in food])
+# score += len(food) - np.min(p2f, axis=0).sum() - min(pac2food)
